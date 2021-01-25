d9f689761b9e8f76dcfdfe255f6e55cc0955b52d
YizYah <yyakovson613@gmail.com>
2021-01-25 18:33:33 +0200

    restored regenerate code call

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /* ns__file unit: standard, comp: src/index.ts */
 /* ns__custom_start beginning */
+import { regenerateCode } from './custom/regenerateCode';
 /* ns__custom_end beginning */
 
 /* types */
 
 /* ns__custom_start export */
-// replace this with your own export
-module.exports = "hello world"
+module.exports = regenerateCode
 /* ns__custom_end export */
